1e5aa4e05cc90bac818e7e0e9af791683e1ad4df
StephanosNikitis <theshaurya035@gmail.com>
2025-07-21 02:06:36 +0530

    Added Codes

--- src/index.js
+++ src/index.js
@@ -10,10 +10,6 @@ app.listen(3000, async () => {
     console.log('Server started')
     await connect()
     console.log('MongoDB Connected');
-    // const tweet = await Tweet.create({
-    //     content: 'Third Tweet',
-    // })
-    // const tweet = await Tweet.find({userEmail: 'a@b.com'})
 
     const tweetRepo = new TweetRepository()
     const tweet = await tweetRepo.getWithComments('687d3e287eb05ce22232006d')
