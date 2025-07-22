3751ff9cd68a8fe3bca250527dce3b307bf03408
StephanosNikitis <theshaurya035@gmail.com>
2025-07-22 15:26:39 +0530

    Added Requirements

--- src/index.js
+++ src/index.js
@@ -12,10 +12,11 @@ app.listen(3000, async () => {
     console.log('MongoDB Connected');
 
     const tweetRepo = new TweetRepository()
-    const tweet = await tweetRepo.getAll(0, 4)
-    if (tweet[0] && tweet[0].contentWithEmail !== undefined) {
-        console.log(tweet[0].contentWithEmail)
-    } else {
-        console.log('contentWithEmail property not found on tweet[0]')
-    }
+    await tweetRepo.create({content: 'With Hooks'})
+    // const tweet = await tweetRepo.getAll(0, 4)
+    // if (tweet[0] && tweet[0].contentWithEmail !== undefined) {
+    //     console.log(tweet[0].contentWithEmail)
+    // } else {
+    //     console.log('contentWithEmail property not found on tweet[0]')
+    // }
 })
