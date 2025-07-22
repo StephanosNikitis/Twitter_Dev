e7bdccf759945d96164866a12eb46a62c0ab1663
StephanosNikitis <theshaurya035@gmail.com>
2025-07-22 16:39:04 +0530

    Added v1 for tweet model and hashtag model

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -4,27 +4,15 @@ const tweetSchema = new mongoose.Schema({
     content: {
         type: String,
         required: true,
+        max: [250, 'Tweet cannot exceed 250 characters']
     },
-    userEmail: {
-        type: String
-    },
-    comments: [
+    hashtags: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comment'
+            ref: 'Hashtag'
         }
     ]
 }, { timestamps: true })
-
-tweetSchema.virtual('contentWithEmail').get(function process(){
-    return `${this.content} \ncreated by: ${this.userEmail}`
-})
-
-tweetSchema.pre('save', function(next) {
-    console.log('Inside a hook');
-    this.content = this.content + '....'
-    next()
-})
 
 const Tweet = mongoose.model('Tweet', tweetSchema)
 
