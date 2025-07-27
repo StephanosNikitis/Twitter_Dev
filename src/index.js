73b0d48f0e60d41d3fc1d83e033aadc91d3b50ac
StephanosNikitis <theshaurya035@gmail.com>
2025-07-27 17:34:02 +0530

    Added tweet service with hashtag processing

--- src/index.js
+++ src/index.js
@@ -3,20 +3,8 @@ const connect = require('./config/database')
 
 const app = express()
 
-const TweetRepository = require('./repository/tweet-repository')
-const Comment = require('./models/comments')
-
 app.listen(3000, async () => {
     console.log('Server started')
     await connect()
     console.log('MongoDB Connected');
-
-    const tweetRepo = new TweetRepository()
-    await tweetRepo.create({content: 'With Hooks'})
-    // const tweet = await tweetRepo.getAll(0, 4)
-    // if (tweet[0] && tweet[0].contentWithEmail !== undefined) {
-    //     console.log(tweet[0].contentWithEmail)
-    // } else {
-    //     console.log('contentWithEmail property not found on tweet[0]')
-    // }
 })
