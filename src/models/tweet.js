ea5f744d6135f23bde7f1c04e91a90dfe4038897
StephanosNikitis <theshaurya035@gmail.com>
2025-08-01 02:17:44 +0530

    Added get tweet with nested comments api

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -17,6 +17,12 @@ const tweetSchema = new mongoose.Schema({
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Like'
         }
+    ],
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
     ]
 }, { timestamps: true })
 
