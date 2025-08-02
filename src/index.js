99df9e9683bf7bfcb55c89ed1d8d200f4b45d6a2
StephanosNikitis <theshaurya035@gmail.com>
2025-08-02 15:34:59 +0530

    Passport implementation with tweet and comment

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import passport from 'passport'
+
 import { connect } from './config/database.js'
+
+import { passportAuth } from './config/jwt-middleware.js'
 
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-import apiRoutes from './routes/index.js'
+app.use(passport.initialize())
+passportAuth(passport)
 
-import { UserRepository, TweetRepository } from './repository/index.js'
-import LikeService from './services/like-service.js'
+import apiRoutes from './routes/index.js'
 
 app.use('/api', apiRoutes)
 
@@ -17,18 +21,4 @@ app.listen(3000, async () => {
     console.log('Server started')
     await connect()
     console.log('MongoDB Connected');
-
-    // const userRepo = new UserRepository()
-    // const tweetRepo = new TweetRepository()
-    // const tweets = await tweetRepo.getAll(0, 10)
-    // const users = await userRepo.getAll()
-
-    // const user = await userRepo.create({
-    //     email: 'shaurya@admin.com',
-    //     password: '1234',
-    //     name: 'Shaurya'
-    // })
-
-    // const likeService = new LikeService()
-    // await likeService.toggleLike(tweets[0].id, 'Tweet', users[0].id)
 })
